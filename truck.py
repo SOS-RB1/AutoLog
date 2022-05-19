a56312f3ce571b0c163736e7920ea406dfdd8e68
SOS-RB1 <100535805+SOS-RB1@users.noreply.github.com>
2022-05-19 22:02:47 +0200

    Update truck.py

--- truck.py
+++ truck.py
@@ -45,7 +45,7 @@ driver.find_element_by_id("example_UserName").send_keys(username_example2)
 driver.find_element_by_id("example_Password").send_keys(password_example2)
 driver.find_element_by_css_selector("example_input[type='submit'][class='submit-button'][value='Log in']").click()
 
-# Opening of the fourth website (opening the website in new tab, click on the login icon login, filling in the login and password fields, click on the login button):
+# Opening of the fourth website (opening the website in new tab, click the login element which will display the login form fields, filling in the login and password fields, click on the login button):
 
 username_example3 = "example_123456"
 password_example3 = "example_qwerty"
